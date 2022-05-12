1fc9fa3ab43e54d9f2eafb63641101748e84f40d
peter_alfie <alfie1104@gmail.com>
2022-05-12 10:30:02 +0900

    feat(auth, auth-form): protecting the "Auth" Page

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
 import { signIn } from "next-auth/client";
 import classes from "./auth-form.module.css";
+import { useRouter } from "next/router";
 
 async function createUser(email, password) {
   const response = await fetch(`/api/auth/signup`, {
@@ -25,6 +26,7 @@ function AuthForm() {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
@@ -43,7 +45,9 @@ function AuthForm() {
         password: enteredPassword,
       });
 
-      console.log(result);
+      if (!result.error) {
+        router.replace("/profile");
+      }
     } else {
       // Input Validation
       // if (
